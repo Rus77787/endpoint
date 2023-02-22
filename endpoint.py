62688d03ed111b6b2ef6c0cf3843582ebdaf513e
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 11:20:18 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,28 +1,20 @@
-
 import streamlit as st
 import requests
 
-BITRIX24_WEBHOOK_URL = "http://endpoint.streamlit.app"
+# Define the endpoint URL for the webhook
+BITRIX_WEBHOOK_URL = "http://endpoint.streamlit.app"
 
-st.title("New Deals Dashboard")
+# Define the event type and data to be sent to the webhook
+EVENT_TYPE = 'new_deal'
+EVENT_DATA = {'deal_name': 'New Deal'}
 
-def get_new_deals():
-    response = requests.get(BITRIX24_WEBHOOK_URL)
+# Create a button in Streamlit to trigger the webhook
+if st.button('Trigger Webhook'):
+    # Send a POST request to the webhook URL with the event type and data
+    response = requests.post(BITRIX_WEBHOOK_URL, json={'event_type': EVENT_TYPE, 'event_data': EVENT_DATA})
+    
+    # Check the status code of the response to ensure the webhook was successful
     if response.status_code == 200:
-        return response.json()
+        st.success('Webhook triggered successfully!')
     else:
-        st.error("Failed to retrieve new deals.")
-
-new_deals = get_new_deals()
-
-if new_deals:
-    st.write(f"Received {len(new_deals)} new deals!")
-    for deal in new_deals:
-        st.write(f"Deal ID: {deal['id']}")
-        st.write(f"Deal Title: {deal['title']}")
-        st.write(f"Deal Value: {deal['value']}")
-else:
-    st.write("No new deals received.")
-
-
-
+        st.error('Failed to trigger webhook.')
