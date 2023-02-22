4ee7b6d5957b69c09c786c9466945832b59a7075
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 12:20:38 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -11,7 +11,6 @@ TOKEN = 'hdejdfhk7gazkb4ukj515towfpqhpnqm'
 
 # Define the event type and data to be sent to the webhook
 EVENT_TYPE = 'ONCRMDEALADD'
-EVENT_DATA = {'deal_name': 'ID'}
 headers = {
     C_REST_CLIENT_ID: C_REST_CLIENT_ID,
     C_REST_CLIENT_SECRET: C_REST_CLIENT_SECRET
@@ -21,7 +20,7 @@ headers = {
 # Create a button in Streamlit to trigger the webhook
 if st.button('Trigger Webhook'):
     # Send a POST request to the webhook URL with the event type and data
-    response = requests.post(BITRIX_WEBHOOK_URL, headers= headers, json={'event_type': EVENT_TYPE, 'event_data': EVENT_DATA})
+    response = requests.post(BITRIX_WEBHOOK_URL, headers= headers, json={'event_type': EVENT_TYPE})
     
     # Check the status code of the response to ensure the webhook was successful
     if response.status_code == 200:
