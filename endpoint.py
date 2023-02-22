3caff3ac57c35b73de0ac71cda53204c4883d8c7
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 08:33:13 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -9,7 +9,7 @@ url = "https://rosasprings.bitrix24.ru/rest/290/webhook/"
 # Payload for the webhook
 payload = {
     "event": "ONCRMPRODUCTADD",
-    "handler": "https://rus77787-endpoint-endpoint-jfv8hn.streamlit.app/",
+    "handler": "https://rus77787-endpoint-endpoint-jfv8hn.streamlit.app",
     "auth": "hdejdfhk7gazkb4ukj515towfpqhpnqm"
 }
 
@@ -21,3 +21,25 @@ def create_webhook():
 # Button to create the webhook
 if st.button('Create Webhook'):
     create_webhook()
+
+    
+    st.title('Bitrix24 Webhook Handler')
+
+@st.cache(allow_output_mutation=True)
+def get_deals():
+    return []
+
+deals = get_deals()
+
+# Endpoint for the webhook handler
+@st.cache(allow_output_mutation=True)
+def webhook_handler(request):
+    data = request.get_json()
+    if data['event'] == 'ONCRMPRODUCTADD':
+        deal = data['data']['FIELDS']
+        deals.append(deal)
+        st.write(deals)
+
+if __name__ == '__main__':
+    st.server.set_app(webhook_handler)
+    st.write('Listening for Bitrix24 webhook events...')
