4ee5fe51d57f69461c81c9545e94f3858dc3564c
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 10:10:27 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,50 +1,15 @@
 import streamlit as st
-from streamlit.web.server.websocket_headers import _get_websocket_headers
-import requests
-import json
+from flask import Flask, request
 
-# Set up the webhook endpoint URL
-webhook_url = "https://endpoint.streamlit.app/"
+app = Flask(__name__)
 
-# Define a function to handle incoming webhook requests
-def handle_webhook(data):
-    # Parse the data from the webhook request
-    payload = json.loads(data)
-    
-    # Check if the payload contains a new deal
-    if payload.get('event') == 'ONCRMDEALADD':
-        deal_id = payload['data'][0]['ID']
-        deal_title = payload['data'][0]['TITLE']
-        st.write(f"New deal added: {deal_title} (ID: {deal_id})")
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    data = request.get_json()
+    st.write(data)
+    return 'success'
 
-# Set up the Streamlit app
-st.title("Bitrix24 Webhook Endpoint")
+if __name__ == '__main__':
+    app.run()
 
-# Listen for incoming webhook requests
-report_ctx = _get_websocket_headers()
-if report_ctx is not None:
-    request_body = report_ctx
-    if request_body:
-        handle_webhook(request_body)
-        
-        st.success("Webhook request handled successfully.")
-else:
-    st.write("Listening for incoming webhook requests...")
-
-# Display a form to test the webhook endpoint
-st.subheader("Test the webhook endpoint")
-deal_title = st.text_input("Deal title")
-if st.button("Simulate new deal"):
-    payload = {
-        "event": "ONCRMDEALADD",
-        "data": [{
-            "ID": "123",
-            "TITLE": deal_title
-        }]
-    }
-    response = requests.post(webhook_url, json=payload)
-    if response.ok:
-        st.success("Webhook request sent successfully.")
-    else:
-        st.error("Failed to send webhook request.")
 
