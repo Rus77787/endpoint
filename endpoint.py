dbad6d3483cf54ee82ac1ed46ec85add532cffbf
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 09:42:43 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -2,20 +2,45 @@ import streamlit as st
 import requests
 import json
 
-st.title('Bitrix24 Webhook')
+# Set up the webhook endpoint URL
+webhook_url = "https://endpoint.streamlit.app/"
 
-# Bitrix24 Webhook URL
-url = "https://rosasprings.bitrix24.ru/rest/290/webhook/"
+# Define a function to handle incoming webhook requests
+def handle_webhook(data):
+    # Parse the data from the webhook request
+    payload = json.loads(data)
+    
+    # Check if the payload contains a new deal
+    if payload.get('event') == 'ONCRMDEALADD':
+        deal_id = payload['data'][0]['ID']
+        deal_title = payload['data'][0]['TITLE']
+        st.write(f"New deal added: {deal_title} (ID: {deal_id})")
 
-# Payload for the webhook
-payload = {
-    "event": "ONCRMPRODUCTADD",
-    "handler": "https://endpoint.streamlit.app/",
-    "auth": "hdejdfhk7gazkb4ukj515towfpqhpnqm"
-}
+# Set up the Streamlit app
+st.title("Bitrix24 Webhook Endpoint")
 
-data = { 'name': 'This is an example for webhook' }
-
-
-
-st.title(requests.post(url, data=json.dumps(data), headers=payload))
+# Listen for incoming webhook requests
+if st._is_running_with_streamlit:
+    if st._is_in_session():
+        st.write("Listening for incoming webhook requests...")
+        request_body = st._request_body
+        if request_body:
+            handle_webhook(request_body)
+            st.success("Webhook request handled successfully.")
+    
+    # Display a form to test the webhook endpoint
+    st.subheader("Test the webhook endpoint")
+    deal_title = st.text_input("Deal title")
+    if st.button("Simulate new deal"):
+        payload = {
+            "event": "ONCRMDEALADD",
+            "data": [{
+                "ID": "123",
+                "TITLE": deal_title
+            }]
+        }
+        response = requests.post(webhook_url, json=payload)
+        if response.ok:
+            st.success("Webhook request sent successfully.")
+        else:
+            st.error("Failed to send webhook request.")
