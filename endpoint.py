c42b769dd482bb23a4379b0732bdad5f1a92beaf
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 11:08:58 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,16 +1,28 @@
 
 import streamlit as st
-from flask import Flask, request, jsonify
+import requests
 
-app = Flask(__name__)
+BITRIX24_WEBHOOK_URL = "http://endpoint.streamlit.app"
 
-@app.route('/bitrix24-webhook', methods=['POST'])
-def bitrix24_webhook():
-    data = request.get_json()
-    st.write(data)
-    return jsonify({'status': 'success'})
+st.title("New Deals Dashboard")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def get_new_deals():
+    response = requests.get(BITRIX24_WEBHOOK_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error("Failed to retrieve new deals.")
+
+new_deals = get_new_deals()
+
+if new_deals:
+    st.write(f"Received {len(new_deals)} new deals!")
+    for deal in new_deals:
+        st.write(f"Deal ID: {deal['id']}")
+        st.write(f"Deal Title: {deal['title']}")
+        st.write(f"Deal Value: {deal['value']}")
+else:
+    st.write("No new deals received.")
+
 
 
