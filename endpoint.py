ae11927ab76f05c03843ac4fb734a0c03eed5f8a
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 12:12:18 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -2,6 +2,9 @@ import streamlit as st
 import requests
 import json
 
+C_REST_CLIENT_ID = 'local.63f5dac3a863a8.66886970'
+C_REST_CLIENT_SECRET = 'pE3n3dSnMTUSO7BhQafuZ0dxJdjRYbaoi0uWHAnlTq0RRt2iKI'
+
 # Define the endpoint URL for the webhook
 BITRIX_WEBHOOK_URL = "http://endpoint.streamlit.app"
 TOKEN = 'hdejdfhk7gazkb4ukj515towfpqhpnqm'
@@ -9,11 +12,16 @@ TOKEN = 'hdejdfhk7gazkb4ukj515towfpqhpnqm'
 # Define the event type and data to be sent to the webhook
 EVENT_TYPE = 'ONCRMDEALADD'
 EVENT_DATA = {'deal_name': 'ID'}
+headers = {
+    C_REST_CLIENT_ID: C_REST_CLIENT_ID,
+    C_REST_CLIENT_SECRET: C_REST_CLIENT_SECRET
+}
+
 
 # Create a button in Streamlit to trigger the webhook
 if st.button('Trigger Webhook'):
     # Send a POST request to the webhook URL with the event type and data
-    response = requests.post(BITRIX_WEBHOOK_URL, json={'event_type': EVENT_TYPE, 'event_data': EVENT_DATA})
+    response = requests.post(BITRIX_WEBHOOK_URL, headers= headers, json={'event_type': EVENT_TYPE, 'event_data': EVENT_DATA})
     
     # Check the status code of the response to ensure the webhook was successful
     if response.status_code == 200:
@@ -21,31 +29,7 @@ if st.button('Trigger Webhook'):
     else:
         st.error('Failed to trigger webhook.')
         
-# Define the route for the webhook endpoint
-@st.cache(allow_output_mutation=True)
-def webhook(request):
-    # Get the JSON data from the request
-    data = json.loads(request)
-    
-    # Check the event type
-    if data['event_type'] == EVENT_TYPE:
-        # Get the data for the new deal event
-        deal_data = data['event_data']
-        
-        # Display the deal data in Streamlit
-        st.write('New deal created:')
-        st.write(deal_data)
-    
-    return 'Webhook received.'
 
-# Run the app
-if __name__ == '__main__':
-    # Get the request data from Bitrix24
-    request = EVENT_TYPE
-    
-    # Trigger the webhook
-    if request:
-        webhook(request)
 
         
 
