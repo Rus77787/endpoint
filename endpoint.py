866bde3bda2420e2f3711b25d74f8f5ca120144b
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-22 09:45:59 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -20,27 +20,29 @@ def handle_webhook(data):
 st.title("Bitrix24 Webhook Endpoint")
 
 # Listen for incoming webhook requests
-if st._is_running_with_streamlit:
-    if st._is_in_session():
-        st.write("Listening for incoming webhook requests...")
-        request_body = st._request_body
-        if request_body:
-            handle_webhook(request_body)
-            st.success("Webhook request handled successfully.")
-    
-    # Display a form to test the webhook endpoint
-    st.subheader("Test the webhook endpoint")
-    deal_title = st.text_input("Deal title")
-    if st.button("Simulate new deal"):
-        payload = {
-            "event": "ONCRMDEALADD",
-            "data": [{
-                "ID": "123",
-                "TITLE": deal_title
-            }]
-        }
-        response = requests.post(webhook_url, json=payload)
-        if response.ok:
-            st.success("Webhook request sent successfully.")
-        else:
-            st.error("Failed to send webhook request.")
+report_ctx = st._get_report_ctx()
+if report_ctx is not None:
+    request_body = report_ctx.request_body_bytes
+    if request_body:
+        handle_webhook(request_body.decode('utf-8'))
+        st.success("Webhook request handled successfully.")
+else:
+    st.write("Listening for incoming webhook requests...")
+
+# Display a form to test the webhook endpoint
+st.subheader("Test the webhook endpoint")
+deal_title = st.text_input("Deal title")
+if st.button("Simulate new deal"):
+    payload = {
+        "event": "ONCRMDEALADD",
+        "data": [{
+            "ID": "123",
+            "TITLE": deal_title
+        }]
+    }
+    response = requests.post(webhook_url, json=payload)
+    if response.ok:
+        st.success("Webhook request sent successfully.")
+    else:
+        st.error("Failed to send webhook request.")
+
