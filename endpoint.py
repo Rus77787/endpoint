0884a8f783e9386719ef184ec97ce3d5be6b222a
Rus77787 <40021542+Rus77787@users.noreply.github.com>
2023-02-23 18:36:15 +0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,29 +1,56 @@
-import streamlit as st
-import requests
 import json
+from botocore.vendored import requests
+from urllib.parse import parse_qs
 
-
-
-C_REST_CLIENT_ID = 'local.63f7262142b291.82315904'
-C_REST_CLIENT_SECRET = 'UHVUSyXlPzaTSASTq5MjOctmz48qsQaEs2FNwW02kaQeXp31F3'
-BITRIX_WEBHOOK_URL = "https://endpoint.streamlit.app"
-
-
-# Define the event type and data to be sent to the webhook
-EVENT_TYPE = 'ONCRMDEALADD'
-
-# Create a button in Streamlit to trigger the webhook
-if st.button('Trigger Webhook'):
-    # Send a POST request to the webhook URL with the event type and data
-    response = requests.post(BITRIX_WEBHOOK_URL, json={'event_type': EVENT_TYPE})
-    print(response.text)
-    # Check the status code of the response to ensure the webhook was successful
-    if response.status_code == 200:
-        st.success('Webhook triggered successfully!')
-    else:
-        st.error('Failed to trigger webhook.')
+def lambda_handler(event, context):
+    
+    data=parse_qs(event['params'])#параметры приходят согласно настройкам меппинга в гетавее
+    event=data['event'][0]
+    key=data['auth[application_token]'][0]
+    
+    #если это наш первый Битрикс24 стучится и если это событие создания нового лида в нем
+    if (event=='ONCRMLEADADD' and key=='81rof1a1dbydthgdl34g5rxkdkqc2mxa'):
         
-
-
+        lid_id=data['data[FIELDS][ID]'][0]
         
-
+        #запросим из первого Б24 остальные параметры лида
+        response = requests.get('https://rosasprings.bitrix24.ru/rest/290/81rof1a1dbydthgdl34g5rxkdkqc2mxa/crm.deal.get',{"id":lid_id})
+        lead_data=response.json()
+        
+        #тут ищу более изящное решение - пока пусть будет так
+        lead_data2 = {
+            "fields" : {
+                "NAME" : lead_data['result']['NAME'],
+                "TITLE" : lead_data['result']['TITLE'],
+                "PHONE" : dict(),
+                "EMAIL" : dict()
+            },
+        'params' : {"REGISTER_SONET_EVENT" : "Y"}
+        }
+        
+        try:
+            phone={
+                "VALUE" : lead_data['result']['PHONE'][0]['VALUE'], 
+                "VALUE_TYPE" : lead_data['result']['PHONE'][0]['VALUE_TYPE']
+                }
+            
+            lead_data2['fields']['PHONE']['0']=phone
+        except:
+            pass
+        
+        try:
+            email={
+                "VALUE" : lead_data['result']['EMAIL'][0]['VALUE'], 
+                "VALUE_TYPE" : lead_data['result']['EMAIL'][0]['VALUE_TYPE']
+                }
+            
+            lead_data2['fields']['EMAIL']['0']=email
+        except:
+            pass
+        
+        
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
